660bd03ab94c74f5a1f3f76b8cb48f5283f02a63
j1yang <106880500+j1yang@users.noreply.github.com>
2022-06-12 15:44:02 -0400

    JS: home fade when scroll down

--- main.js
+++ main.js
@@ -29,6 +29,19 @@ contact.addEventListener('click', (event)=>{
   scrollIntoView('#contact');
 });
 
+// Make home slowly fade when scroll down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+  home.style.opacity = 1 - window.scrollY/(homeHeight);
+});
+
+
+// Show Arrow-Up button when scrolling
+document.addEventListener('scroll',()=>{
+  if(window.home){}
+});
+
 function scrollIntoView(selector){
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: 'smooth'});
