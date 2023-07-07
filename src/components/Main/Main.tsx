ce4f7730a667057ae0b9b909bc8b36b0c968398e
Jay Yang <106880500+j1yang@users.noreply.github.com>
2023-07-07 18:16:35 -0400

    animation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,85 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import Introduction from './Introduction/Introduction';
 import Projects from './Projects/Projects';
 import TechStacks from '../TechStacks/TechStacks';
 import Contacts from '../Contacts/Contacts';
 
-interface MainProps {
-}
+gsap.registerPlugin(ScrollTrigger);
 
-const Main: React.FC<MainProps> = ({ }) => {
+interface MainProps {}
+
+const Main: React.FC<MainProps> = () => {
+  const introductionRef = useRef<HTMLDivElement>(null);
+  const techStacksRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const animateOnScroll = (ref: React.RefObject<HTMLDivElement>, animation: any) => {
+      if (ref.current) {
+        gsap.to(ref.current, {
+          opacity: 1,
+          x: 0, // Change y to x for horizontal animation
+          duration: 1,
+          scrollTrigger: {
+            trigger: ref.current,
+            start: 'top 80%',
+            end: 'bottom 80%',
+            toggleActions: 'play none none reverse',
+          },
+          onComplete: () => {
+            animation.kill();
+          },
+        });
+      }
+    };
+
+    const introductionAnimation = gsap.from(introductionRef.current, {
+      opacity: 0,
+      y: 100,
+      duration: 1,
+    });
+    animateOnScroll(introductionRef, introductionAnimation);
+
+    const techStacksAnimation = gsap.from(techStacksRef.current, {
+      opacity: 0,
+      y: 100,
+      duration: 1,
+    });
+    animateOnScroll(techStacksRef, techStacksAnimation);
+
+    const projectsAnimation = gsap.from(projectsRef.current, {
+      opacity: 0,
+      y: 100,
+      duration: 1,
+    });
+    animateOnScroll(projectsRef, projectsAnimation);
+
+    const contactsAnimation = gsap.from(contactsRef.current, {
+      opacity: 0,
+      x: -100, // Animation from left side
+      duration: 1,
+    });
+    animateOnScroll(contactsRef, contactsAnimation);
+  }, []);
 
   return (
     <main>
-      <Introduction />
-      <TechStacks />
-      <Projects />
-      <Contacts />
+      <div className='mb-[12em]' ref={introductionRef}>
+        <Introduction />
+      </div>
+      <div ref={techStacksRef}>
+        <TechStacks />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+      <div ref={contactsRef}>
+        <Contacts />
+      </div>
     </main>
   );
 };
