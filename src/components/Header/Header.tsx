a15a1f75818f9a76fb36e30aba5ac00253940ce6
Jay Yang <106880500+j1yang@users.noreply.github.com>
2023-08-29 19:44:27 -0400

    View Resume added to nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,19 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = () => {
+  // const handleDownload = () => {
+  //   const link = document.createElement('a');
+  //   link.href = '/resume.pdf'; // Replace with the actual path to your PDF file
+  //   link.download = 'JaewonYang_Resume.pdf'; // Change the downloaded file name if needed
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // };
+  const openPdfInNewTab = () => {
+    window.open('/portfolio/jaewonYang_resume.pdf', '_blank'); // Replace with the actual path to your PDF file
+  };
+
+
   const handleNavigation = (event:any, targetId:string) => {
     event.preventDefault();
     const targetElement = document.getElementById(targetId);
@@ -15,6 +28,7 @@ const Header: React.FC<HeaderProps> = () => {
       inline: 'nearest',
     });
   };
+
   return (
     <header className='flex justify-between py-7'>
       <h1 className="text-5xl font-extrabold">
@@ -34,12 +48,9 @@ const Header: React.FC<HeaderProps> = () => {
         <li className="mr-6">
           <a className="text-2xl link link-underline link-underline-black leading-tight" onClick={(e) => handleNavigation(e, 'Contacts')} href="#Contacts">Contact me</a>
         </li>
-        {/* <li className="mr-6">
-          <a className="text-2xl link link-underline link-underline-black leading-tight" target='_blank' href="https://www.linkedin.com/in/jaewon-yang/">LinkedIn</a>
-        </li>
         <li className="mr-6">
-          <a className="text-2xl link link-underline link-underline-black leading-tight" target='_blank' href="https://github.com/j1yang">Github</a>
-        </li> */}
+          <a className="text-2xl link link-underline link-underline-black leading-tight cursor-pointer" onClick={() => openPdfInNewTab()} >Resume</a>
+        </li>
       </ul>
     </header>
   );
