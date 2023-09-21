8a6274d349fe9601e5e8ad4b7d33eaf5bfcc7d30
Jay Yang <106880500+j1yang@users.noreply.github.com>
2023-09-21 11:22:27 -0400

    path changed

--- src/portfolio.ts
+++ src/portfolio.ts
@@ -15,7 +15,7 @@ const portfolio: Project[] = [
     id: 'project1',
     title: 'Motion Tracking for Avatar',
     description: 'Aimed at revolutionizing the desktop experience of the metaverse, uses THREE.js and advanced motion tracking technology MediaPipe from Google. The end goal was to provide a highly immersive learning experience for desktop users. I implemented a system for bone interpolation that could process webcam feeds in real time, translating users\' physical movements into the digital world. ',
-    imageUrl: '/motiontracking.png',
+    imageUrl: 'motiontracking.png',
     githubLink: 'https://github.com/username/project1',
     websiteLink: 'https://project1.example.com',
     css:''
@@ -24,7 +24,7 @@ const portfolio: Project[] = [
     id: 'project2',
     title: 'Arm Inverse Kinematic',
     description: 'In this innovative project, I utilized Inverse Kinematics (IK) and THREE.js to create authentic arm movements for VR avatars, delivering an immersive user experience in the webxr class. Leveraging JavaScript, I facilitated real-time calculations for intricate avatar control. By applying Euler & quaternion mathematics, I managed complex rotations, demonstrating my prowess in 3D programming.',
-    imageUrl: '/vrik1.png',
+    imageUrl: 'vrik1.png',
     githubLink: 'https://github.com/username/project1',
     websiteLink: 'https://project1.example.com',
     css:'flex-row-reverse'
@@ -33,7 +33,7 @@ const portfolio: Project[] = [
     id: 'project3',
     title: 'Travel Journey with globe',
     description: 'Interactive globe with THREE.js. Users could traverse the globe, I leveraged the versatility of React to build a user-friendly front end, GSAP animations were used to enhance the UI/UX, providing visually pleasing transitions and interactive elements. The application was written in TypeScript and utilized Vite as the build tool, showcasing my proficiency in modern JavaScript frameworks and tools.',
-    imageUrl: '/threetravel.png',
+    imageUrl: 'threetravel.png',
     githubLink: 'https://github.com/username/project1',
     websiteLink: 'https://project1.example.com',
     css:''
